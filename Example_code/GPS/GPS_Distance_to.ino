c1bea11b86f7c88917966d0bed2a90a956172d22
LuPow132 <68886157+LuPow132@users.noreply.github.com>
2024-01-07 22:16:06 +0700

    Create GPS_Distance_to.ino

---
+++ Example_code/GPS/GPS_Distance_to.ino
@@ -0,0 +1,39 @@
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+
+static const int RXPin = 11, TXPin = 10;
+static const uint32_t GPSBaud = 9600;
+
+// The TinyGPS++ object
+TinyGPSPlus gps;
+
+// The serial connection to the GPS device
+SoftwareSerial ss(RXPin, TXPin);
+
+float landingLat = 13.277975;
+float landingLng = 100.923488;
+
+void setup() {
+  Serial.begin(115200);
+  ss.begin(GPSBaud);
+}
+
+void loop() {
+  // This sketch displays information every time a new sentence is correctly encoded.
+  while (ss.available() > 0) {
+    gps.encode(ss.read());
+    if (gps.location.isUpdated()) {
+      if(gps.satellites.value() > 9){
+        Serial.println(TinyGPSPlus::distanceBetween(gps.location.lat(), gps.location.lng(), landingLat, landingLng));
+        // Latitude in degrees (double)
+        Serial.print(gps.location.lat(), 6);
+        // Longitude in degrees (double)
+        Serial.print(",");
+        Serial.println(gps.location.lng(), 6);
+      }
+
+      // Serial.print("Number os satellites in use = ");
+      // Serial.println(gps.satellites.value());
+    }
+  }
+}
