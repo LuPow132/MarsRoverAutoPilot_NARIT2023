cebce0eea4daac7497ec373e9be69093e539b6fe
LuPow132 <68886157+LuPow132@users.noreply.github.com>
2024-02-02 21:41:53 +0700

    Update recieve_GPS.ino

--- Example_code/GPS/Communication/recieve_GPS.ino
+++ Example_code/GPS/Communication/recieve_GPS.ino
@@ -1,9 +1,11 @@
 #include <SPI.h>  
 #include "RF24.h" 
-
+#include <TinyGPS++.h>
 RF24 myRadio (7, 8); 
 byte addresses[][6] = {"0"};
 
+double landing_lat = 13.276444;
+double landing_long =  100.921656;
 struct package {
   int id=0;
   double rover_lat = 0.0;
@@ -36,9 +38,11 @@ void loop()  {
     Serial.print("\nPackage:");
     Serial.print(data.id);
     Serial.print("\n");
-    Serial.println(data.rover_lat);
-    Serial.println(data.rover_long);
+    Serial.println(data.rover_lat, 6);
+    Serial.println(data.rover_long, 6);
     Serial.println(data.sat_used);
+    Serial.print("The distance from local to GPS is: ");
+    Serial.println(TinyGPSPlus::distanceBetween(landing_lat, landing_long  , data.rover_lat,  data.rover_long));
 
   }
 }
