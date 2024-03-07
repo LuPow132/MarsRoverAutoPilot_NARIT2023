e99dfb75659137d0ac70df6c3abecf2b74fa65e9
LuPow132 <68886157+LuPow132@users.noreply.github.com>
2024-03-07 21:31:18 +0700

    Update PingAll.ino

--- Example_code/Ultrasonic/PingAll.ino
+++ Example_code/Ultrasonic/PingAll.ino
@@ -1,13 +1,3 @@
-
-
-/*
-  Ultrasonic Sensor HC-SR04 and Arduino Tutorial
-
-  by Dejan Nedelkovski,
-  www.HowToMechatronics.com
-
-*/
-// defines pins numbers
 const int trigPin_ut_1 = 22;
 const int echoPin_ut_1 = 23;
 int distance_1;
@@ -24,63 +14,41 @@ const int trigPin_ut_4 = 28;
 const int echoPin_ut_4 = 29;
 int distance_4;
 
-// defines variables
 long duration;
 int distance;
+int trig,echo;
 
-int ping(int trigPin,int echoPin){
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
-  // Calculating the distance
-  distance = duration * 0.034 / 2;
+int ping(int trig,int echo){
+  digitalWrite(trig, LOW); 
+  delayMicroseconds(5); 
+  digitalWrite(trig, HIGH); 
+  delayMicroseconds(5); 
+  digitalWrite(trig, LOW); //ใช้งานขา trig
+  
+  duration = pulseIn(echo, HIGH); //อ่านค่าของ echo
+  distance = (duration/2) / 29.1; //คำนวณเป็น centimeters
   return distance;
 }
 
-void pingAll(){
-  distance_1 = ping(trigPin_ut_1,echoPin_ut_1);
-  delay(10);
-  distance_2 = ping(trigPin_ut_2,echoPin_ut_2);
-  delay(10);
-  distance_3 = ping(trigPin_ut_3,echoPin_ut_3);
-  delay(10);
-  distance_4 = ping(trigPin_ut_4,echoPin_ut_4);
-  delay(10);
-}
-
 void setup() {
-  pinMode(trigPin_ut_1, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin_ut_1, INPUT); // Sets the echoPin as an Input
-
-  pinMode(trigPin_ut_2, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin_ut_2, INPUT); // Sets the echoPin as an Input
-
-  pinMode(trigPin_ut_3, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin_ut_3, INPUT); // Sets the echoPin as an Input
-
-  pinMode(trigPin_ut_4, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin_ut_4, INPUT); // Sets the echoPin as an Input
-
-  Serial.begin(9600); // Starts the serial communication
+  Serial.begin(9600);
+  pinMode(echoPin_ut_1, INPUT); //สั่งให้ขา echo ใช้งานเป็น input
+  pinMode(trigPin_ut_1, OUTPUT); //สั่งให้ขา trig ใช้งานเป็น output
+  pinMode(echoPin_ut_2, INPUT); //สั่งให้ขา echo ใช้งานเป็น input
+  pinMode(trigPin_ut_2, OUTPUT); //สั่งให้ขา trig ใช้งานเป็น output
+  pinMode(echoPin_ut_3, INPUT); //สั่งให้ขา echo ใช้งานเป็น input
+  pinMode(trigPin_ut_3, OUTPUT); //สั่งให้ขา trig ใช้งานเป็น output
+  pinMode(echoPin_ut_4, INPUT); //สั่งให้ขา echo ใช้งานเป็น input
+  pinMode(trigPin_ut_4, OUTPUT); //สั่งให้ขา trig ใช้งานเป็น output
 }
+
 void loop() {
-  pingAll();
-  Serial.print("D1_");
-  Serial.println(distance_1);
-
-  Serial.print("D2_");
-  Serial.println(distance_2);
-
-  Serial.print("D3_");
-  Serial.println(distance_3);
-
-  Serial.print("D4_");
-  Serial.println(distance_4);
-  
-  Serial.println("======================");
+  Serial.print(ping(trigPin_ut_1,echoPin_ut_1)); 
+  Serial.print(" | ");
+  Serial.print(ping(trigPin_ut_2,echoPin_ut_2)); 
+  Serial.print(" | ");
+  Serial.print(ping(trigPin_ut_3,echoPin_ut_3)); 
+  Serial.print(" | ");
+  Serial.println(ping(trigPin_ut_4,echoPin_ut_4)); 
+  delay(100);
 }
